c7157249c7e70a8fe3961dfc00e59a5f4160200f
Donald Huang <don.hcd@gmail.com>
2015-03-10 03:22:22 +0000

    add subjectAltName

--- ldap-base/run.sh
+++ ldap-base/run.sh
@@ -10,7 +10,13 @@ ln -s /data/ldap /var/lib/ldap
 mkdir /etc/ldap
 openssl genrsa -out /etc/ldap/private.pem 2048
 openssl req -new -x509 -key /etc/ldap/private.pem -out /etc/ldap/cert.pem -days 1095 \
-  -subj '/CN=localhost/O=docker/C=US'
+  -subj '/CN=localhost/O=docker/C=US' \
+  -config <( \
+  cat <<-EOF \
+  [v3_ca] \
+  subjectAltName = IP:52.10.49.1 \
+  EOF \
+  )
 
 echo -n "TLSCipherSuite HIGH:MEDIUM:+TLSv1:!SSLv2:+SSLv3 \
 TLSCACertificateFile /etc/ldap/cert.pem \
