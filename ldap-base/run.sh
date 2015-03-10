00c951d76847e2c4569d57cdbb82669da4a6ac36
Donald Huang <don.hcd@gmail.com>
2015-03-10 00:59:37 +0000

    serve ldaps

--- ldap-base/run.sh
+++ ldap-base/run.sh
@@ -7,5 +7,15 @@ else
 fi
 
 ln -s /data/ldap /var/lib/ldap
+mkdir /etc/ldap
+openssl genrsa -out /etc/ldap/private.pem 2048
+openssl req -new -x509 -key /etc/ldap/private.pem -out /etc/ldap/cert.pem -days 1095 \
+  -subj '/CN=localhost/O=docker/C=US'
 
-exec /usr/sbin/slapd -u ldap -d 3
+echo -n "TLSCipherSuite HIGH:MEDIUM:+TLSv1:!SSLv2:+SSLv3 \
+TLSCACertificateFile /etc/ldap/cert.pem \
+TLSCertificateFile /etc/ldap/cert.pem \
+TLSCertificateKeyFile /etc/ldap/private.pem \
+TLSVerifyClient never" > /etc/ldap/slapd.conf
+
+exec /usr/sbin/slapd -h 'ldap:// ldaps://' -u ldap -d 3
